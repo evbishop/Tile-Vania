a982ce205b71e2aaac8f9f440c8b46a3fb7da724
evbishop <anddenissyuk@gmail.com>
2022-01-12 18:12:30 +0600

    Fixed bug of coin being collected twice

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -7,13 +7,15 @@ public class Coin : MonoBehaviour
 {
     [SerializeField] AudioClip coinSound;
     [SerializeField] int coinValue = 100;
-
+    bool coinEnabled = true;
     public static event Action<int> OnCoinTaken;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!coinEnabled) return;
         AudioSource.PlayClipAtPoint(coinSound, Camera.main.transform.position);
         OnCoinTaken?.Invoke(coinValue);
         Destroy(gameObject);
+        coinEnabled = false;
     }
 }
